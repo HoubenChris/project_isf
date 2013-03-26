0be2eab65a8b6e73eb33a064048e75fba35b8927
Maarten Anckaert <maarten.anckaert@gmail.com>
2013-03-26 15:47:31 +0100

    commit NSF

---
+++ project_isf/project_isf.Domain/POCO/NSF.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_isf.Domain.POCO
+{
+  public class NSF
+  {
+    public int NSFId { get; set; }
+    public string Country { get; set; }
+    public string Region { get; set; }
+  }
+}
