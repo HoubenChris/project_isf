6693d3fb78f9186b95b15f59a3d308b0f321d591
Vincent Huysmans <vinny_wijnegem@hotmail.com>
2013-03-26 15:44:25 +0100

    klasse Team

---
+++ project_isf/project_isf.Domain/POCO/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_isf.Domain.POCO
+{
+    class Team
+    {
+        public int TeamId { get; set; }
+        public int CategoryId { get; set; }
+        public int CoachId { get; set; }
+        public int SchoolId { get; set; }
+        public string TeamNaam { get; set; }
+        public Boolean Category { get; set; }
+
+        public virtual Coach Coach { get; set; }
+        public virtual School School { get; set; }
+
+    }
+}
