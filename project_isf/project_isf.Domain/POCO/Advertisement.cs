3b7cb2b4f1642d4d7c67629585266d176618a2ed
Maarten Anckaert <maarten.anckaert@gmail.com>
2013-03-26 15:45:27 +0100

    commit advertisement

---
+++ project_isf/project_isf.Domain/POCO/Advertisement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_isf.Domain.POCO
+{
+  public class Advertisement
+  {
+    public int AdvertisementId { get; set; }
+    public string Company { get; set; }
+    public int AdvertisementLocationId { get; set; }
+
+    public virtual AdvertisementLocation AdvertisementLocation { get; set; }
+  }
+}
---
+++ project_isf/project_isf.Domain/POCO/AdvertisementLocation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_isf.Domain.POCO
+{
+  public class AdvertisementLocation
+  {
+    public int AdvertisementLocationId { get; set; }
+    public int LocationId { get; set; }
+    public int AdvertisementId { get; set; }
+
+    public virtual Location Location { get; set; }
+    public virtual Advertisement Advertisement { get; set; }
+  }
+}
