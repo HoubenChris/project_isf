9e24073b76661d7b303386fd09c3eb870c79bea4
Vincent Huysmans <vinny_wijnegem@hotmail.com>
2013-03-27 18:53:02 +0100

    check description part 2

--- project_isf/project_isf.Domain/Concrete/EFDbContext.cs
+++ project_isf/project_isf.Domain/Concrete/EFDbContext.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
+using project_isf.Domain.POCO;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace project_isf.Domain.Concrete
 {
-    class EFDbContext
+    public class EFDbContext: DbContext
     {
+        public DbSet<Advertisement> Advertisements { get; set; }
+        public DbSet<AdvertisementLocation> AdvertisementLocations { get; set; }
+        public DbSet<Coach> Coaches { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<ISFRepresentative> ISFRepresantives { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Meeting> Meetings { get; set; }
+        public DbSet<MeetingEvent> MeetingEvents { get; set; }
+        public DbSet<NSF> NSFs { get; set; }
+        public DbSet<NSFRepresentative> NSFRepresentatives { get; set; }
+        public DbSet<School> Schools { get; set; }
+        public DbSet<SchoolCupCoordinator> SchoolCupCoordinators { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Team> Teams { get; set; }
+
+        //voorkomen dat EF automatisch "s" toevoegd aan de naam van het model
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
